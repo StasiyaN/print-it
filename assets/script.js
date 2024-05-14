5dcc8ad9e308e43a3f3bb4795976d8dd5a9545df
StasiyaN <ana.nyzhalska@gmail.com>
2024-05-14 13:34:57 +0200

    ajout de la ligne dot color pour chaque objet dans le slider, suppression du alt initial et ajout du alt nouveau

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,29 @@ const slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
-		"altText": "Impressions tous formats en boutique et en ligne"
+		"altText": "Impressions tous formats en boutique et en ligne",
+		"dotColor": "aqua"
 
 	},
 	{
 		"image":"slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-		"altText": "Tirages haute définition grand format pour vos bureaux et events"
+		"altText": "Tirages haute définition grand format pour vos bureaux et events",
+		"dotColor": "pink"
 
 	},
 	{
 		"image":"slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-		"altText": "Grand choix de couleurs de CMJN aux pantones"
+		"altText": "Grand choix de couleurs de CMJN aux pantones",
+		"dotColor": "paleturquoise"
 
 	},
 	{
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
-		"altText": "Autocollants avec découpe laser sur mesure"
+		"altText": "Autocollants avec découpe laser sur mesure",
+		"dotColor": "lavender"
 
 	}
 ]
@@ -29,10 +33,12 @@ const arrowRight = document.querySelector('.arrow_right');
 const arrowLeft = document.querySelector('.arrow_left');
 //AJOUT BULLET POINTS
 const dots = document.querySelector('.dots');
-//RECUPERATION DE BANNER
-let slider = document.querySelector('.banner-img');
 //CREATION DU VARIABLE SLIDE EN COURS  DANS LA DIAPO
 let curImageIndex = 0;
+//RECUPERATION DE BANNER + SUPPRESION DE ALT INITIAL + AJOUT DE NOUVEAU ATL
+let slider = document.querySelector('.banner-img');
+slider.removeAttribute('alt');
+slider.setAttribute('alt, Impressions tous formats en boutique et en ligne');
 
 
 //FONCTION UPDATE BULLET POINTS
