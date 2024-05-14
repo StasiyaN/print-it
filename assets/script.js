38ab4a77ab8482c608760073c4bcaf67ae2faeae
StasiyaN <ana.nyzhalska@gmail.com>
2024-05-14 12:25:56 +0200

    amelioration de la structure du code

--- assets/script.js
+++ assets/script.js
@@ -16,85 +16,85 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
+//RECUPERATION DES FLECHES
+const arrowRight = document.querySelector('.arrow_right');
+const arrowLeft = document.querySelector('.arrow_left');
+//AJOUT BULLET POINTS
+const dots = document.querySelector('.dots');
+//RECUPERATION DE BANNER
+let slider = document.querySelector('.banner-img');
+//CREATION DU VARIABLE SLIDE EN COURS  DANS LA DIAPO
+let curImageIndex = 0;
 
 
 //FONCTION UPDATE BULLET POINTS
 function updateDots () {
 	const dotElements = document.querySelectorAll('.dot');
-		dotElements.forEach ((dot, index) => {
-			if (index === curImageIndex) {
-				dot.classList.add ('dot_selected');
-			} else {
-				dot.classList.remove ('dot_selected');
-			}
-		});
+	dotElements.forEach ((dot, index) => {
+		if (index === curImageIndex) {
+			dot.classList.add ('dot_selected');
+		} else {
+			dot.classList.remove ('dot_selected');
+		}
+	});
 }
 
 updateDots();
 
- //  //AJOUT BULLET POINTS
-const dots = document.querySelector('.dots');
+
 
 slides.forEach(() => {
-let dotsList = document.createElement('span');
-dotsList.classList.add('dot');
-dots.appendChild(dotsList);
+	let dotsList = document.createElement('span');
+	dotsList.classList.add('dot');
+	dots.appendChild(dotsList);
 });
 
 //  console.log(dots);
 
 
- //RECUPERATION DE BANNER
-let slider = document.querySelector('.banner-img');
 
 
- //CREATION DU VARIABLE SLIDE EN COURS  DANS LA DIAPO
-let curImageIndex = 0;
+
 
 
 //AJOUT DE LA STUCTURE DU SLIDER DANS BANNER
 function slideShow (index) {
 	if (slides[index]) { // amelioration du code en simplifiant la ligne (i)ndex >= 0 && index < slides.length) 
-        slider.src = `./assets/images/slideshow/${slides[index].image}`;
-        document.querySelector('#banner p').innerHTML = slides[index].tagLine;
-    
-	updateDots();
+		slider.src = `./assets/images/slideshow/${slides[index].image}`;
+		document.querySelector('#banner p').innerHTML = slides[index].tagLine;
+		
+		updateDots();
 	}
 }
 
-//RECUPERATION DES FLECHES
 
-const arrowRight = document.querySelector('.arrow_right');
-
-const arrowLeft = document.querySelector('.arrow_left');
 
 // AJOUT DE EVENT LISTENERS POUR LES FLECHES & CREATION DE LA FONCTIONS CLICK RIGHT & LEFT
 
 arrowRight.addEventListener('click', () => {
-    curImageIndex += 1;
-
-    if (curImageIndex >= slides.length) {
-        curImageIndex = 0; 
-    }
-
-    slideShow(curImageIndex);
+	curImageIndex += 1;
+	
+	if (curImageIndex >= slides.length) {
+		curImageIndex = 0; 
+	}
+	
+	slideShow(curImageIndex);
 });
 
 arrowLeft.addEventListener('click', () => {
-    curImageIndex -= 1;
-
-    if (curImageIndex < 0) {
-        curImageIndex = slides.length - 1; 
-    }
-
-    slideShow(curImageIndex);
+	curImageIndex -= 1;
+	
+	if (curImageIndex < 0) {
+		curImageIndex = slides.length - 1; 
+	}
+	
+	slideShow(curImageIndex);
 });
 
 
 
 
-	
+
 
 
 
