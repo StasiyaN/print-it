745431d6da3ec2b9d3da311333d7c027d8a5a34f
StasiyaN <ana.nyzhalska@gmail.com>
2024-05-14 14:14:43 +0200

    ajout de curseur pointer pour les fleches

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,18 @@ const slides = [
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
 		"altText": "Impressions tous formats en boutique et en ligne",
 		"dotColor": "aqua"
-
 	},
 	{
 		"image":"slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 		"altText": "Tirages haute définition grand format pour vos bureaux et events",
 		"dotColor": "pink"
-
 	},
 	{
 		"image":"slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
 		"altText": "Grand choix de couleurs de CMJN aux pantones",
 		"dotColor": "paleturquoise"
-
 	},
 	{
 		"image":"slide4.png",
@@ -27,9 +24,12 @@ const slides = [
 		"dotColor": "lavender"
 	}
 ]
-//RECUPERATION DES FLECHES
+//RECUPERATION DES FLECHES + AJOUT DE CURSEUR POINTER
 const arrowRight = document.querySelector('.arrow_right');
+arrowRight.style.cursor = 'pointer';
 const arrowLeft = document.querySelector('.arrow_left');
+arrowLeft.style.cursor = 'pointer';
+
 //AJOUT BULLET POINTS
 const dots = document.querySelector('.dots');
 //CREATION DU VARIABLE SLIDE EN COURS  DANS LA DIAPO
@@ -38,7 +38,6 @@ let curImageIndex = 0;
 let slider = document.querySelector('.banner-img');
 slider.removeAttribute('alt');
 slider.setAttribute('alt', 'Impressions tous formats en boutique et en ligne');
-
 
 //FONCTION POUR COLORER LES DOTS A CHAQUE SLIDE
 function colorDots () {
@@ -51,47 +50,35 @@ function colorDots () {
 		}
 	});
 }
-
 colorDots();
-
-
 
 slides.forEach(() => {
 	let dotsList = document.createElement('span');
 	dotsList.classList.add('dot');
 	dots.appendChild(dotsList);
 });
-
-
-
 //AJOUT DE LA STUCTURE DU SLIDER DANS BANNER
 function slideShow (index) {
 	if (slides[index]) { // amelioration du code en simplifiant la ligne (i)ndex >= 0 && index < slides.length) 
 		slider.src = `./assets/images/slideshow/${slides[index].image}`;
 		document.querySelector('#banner p').innerHTML = slides[index].tagLine;		
-		colorDots();
+colorDots();
 	}
 }
 
 // AJOUT DE EVENT LISTENERS POUR LES FLECHES & CREATION DE LA FONCTIONS CLICK RIGHT & LEFT
-
 arrowRight.addEventListener('click', () => {
 	curImageIndex += 1;
-	
 	if (curImageIndex >= slides.length) {
 		curImageIndex = 0; 
 	}
-	
 	slideShow(curImageIndex);
 });
-
 arrowLeft.addEventListener('click', () => {
 	curImageIndex -= 1;
-	
 	if (curImageIndex < 0) {
 		curImageIndex = slides.length - 1; 
 	}
-	
 	slideShow(curImageIndex);
 });
 
