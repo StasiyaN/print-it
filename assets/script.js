8253e9cb673ae7ffab90bd83f00cf66bef441dd4
StasiyaN <ana.nyzhalska@gmail.com>
2024-05-16 11:43:21 +0200

    touche final - modification de couleur de bullet points - couleurs du banner :)

--- assets/script.js
+++ assets/script.js
@@ -9,19 +9,19 @@ const slides = [
 		"image":"slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 		"altText": "Tirages haute définition grand format pour vos bureaux et events",
-		"dotColor": "pink"
+		"dotColor": "#CD006B"
 	},
 	{
 		"image":"slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
 		"altText": "Grand choix de couleurs de CMJN aux pantones",
-		"dotColor": "paleturquoise"
+		"dotColor": "#eee425"
 	},
 	{
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
 		"altText": "Autocollants avec découpe laser sur mesure",
-		"dotColor": "lavender"
+		"dotColor": "#3396c0"
 	}
 ]
 //********DECLARATION DES VARIABLES******
